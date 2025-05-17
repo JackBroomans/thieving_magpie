1f47bd9f380bcb9fe20e937353e0a852e7b04041
Jack Broomans <jack.broomans@outlook.com>
2025-05-17 17:39:33 +0200

    Initiële commit Java backend

---
+++ src/main/java/nl/schuldhulp/schuldhulp/SchuldhulpApplication.java
@@ -0,0 +1,13 @@
+package nl.schuldhulp.schuldhulp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchuldhulpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchuldhulpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/schuldhulp/schuldhulp/SchuldhulpApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.schuldhulp.schuldhulp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchuldhulpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
